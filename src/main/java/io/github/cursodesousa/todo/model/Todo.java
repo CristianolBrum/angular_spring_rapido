e846e4965baf005b1119a91ffbdef07eabde27e2
Cristiano Brum <Cristianolbrum@gmail.com>
2022-03-15 18:57:37 -0400

    aula 07 - criando repositorio para manipular entidades

--- src/main/java/io/github/cursodesousa/todo/model/Todo.java
+++ src/main/java/io/github/cursodesousa/todo/model/Todo.java
@@ -8,12 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class Todo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +26,46 @@ public class Todo {
 	
 	@Column
 	private LocalDateTime doneDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getDoneDate() {
+		return doneDate;
+	}
+
+	public void setDoneDate(LocalDateTime doneDate) {
+		this.doneDate = doneDate;
+	}
+
+	
 }
