002771a303834e4010f525f22f85e5dcf0206af1
Cristiano Brum <Cristianolbrum@gmail.com>
2022-03-21 20:06:05 -0400

    aula 20 - criando a listagem

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Todo } from './todo';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todos: Todo[] = []; 
   form: FormGroup = new FormGroup({
     description : new FormControl('')  
   })
@@ -20,7 +21,11 @@ constructor(private service : TodoService){
 submit(){
     console.log(this.form.value)
     const todo: Todo = {...this.form.value}
-    this.service.salvar(todo)
-    .subscribe(todo => console.log(todo))
+    this.service
+    .salvar(todo)
+    .subscribe(savedTodo => {
+      this.todos.push(savedTodo)
+      this.form.reset()
+    })
   }
 }
