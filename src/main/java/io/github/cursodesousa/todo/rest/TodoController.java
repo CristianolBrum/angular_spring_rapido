f5ae0536d51e6f50a33ad3fd40f4d501cbdf3d5f
Cristiano Brum <Cristianolbrum@gmail.com>
2022-03-22 17:55:06 -0400

    aula 27 - adicinando arquivo system.properties para heroku

--- src/main/java/io/github/cursodesousa/todo/rest/TodoController.java
+++ src/main/java/io/github/cursodesousa/todo/rest/TodoController.java
@@ -18,45 +18,43 @@ import org.springframework.web.server.ResponseStatusException;
 
 import io.github.cursodesousa.todo.model.Todo;
 import io.github.cursodesousa.todo.repository.TodoRepository;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
-	
+
 	@Autowired
-	private TodoRepository todoRepository;  
+	private TodoRepository todoRepository;
 
 	@PostMapping
 	public Todo save(@RequestBody Todo todo) {
 		return todoRepository.save(todo);
 	}
-	
+
 	@GetMapping
-	public List<Todo> getAll(){
+	public List<Todo> getAll() {
 		return todoRepository.findAll();
 	}
-	
+
 	@GetMapping("{id}")
 	public Todo getById(@PathVariable Long id) {
-		return todoRepository
-				.findById(id)
-				.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		return todoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@DeleteMapping("{id}")
 	public void delete(@PathVariable Long id) {
-		todoRepository
-		.deleteById(id);
+		todoRepository.deleteById(id);
 	}
-	
+
 	@PatchMapping("{id}/done")
 	public Todo markAsDone(@PathVariable Long id) {
-		return todoRepository.findById(id).map(todo ->{
+		return todoRepository.findById(id).map(todo -> {
 			todo.setDone(true);
 			todo.setDoneDate(LocalDateTime.now());
 			todoRepository.save(todo);
 			return todo;
 		}).orElse(null);
-		
+
 	}
 }
